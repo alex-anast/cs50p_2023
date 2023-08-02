11c5a1838dc7d3aeba3ed8059c43b6e9b9b21063
alex-anast <anastasioyaa@gmail.com>
2023-08-02 20:49:12 +0200

    7 numb3rs

---
+++ 7_regex/numb3rs/numb3rs.py
@@ -0,0 +1,41 @@
+"""
+Implement a function called validate that expects an IPv4 address
+as input as a str and then returns True or False, respectively,
+if that input is a valid IPv4 address or not.
+
+Additionally implement two or more functions that collectively
+test your implementation of validate thoroughly.
+"""
+
+
+import re
+import sys
+
+
+def main():
+    try:
+        print(validate(input("IPv4 Address: ")))
+    except:
+        sys.exit("Exited with error.")
+
+
+def validate(ip):
+    # match the range 0-255 four times with a dot in-between
+    is_valid = re.search(
+        r"^(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)\." +
+        r"(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)\." +
+        r"(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)\." +
+        r"(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)$",
+        ip
+    )
+    if is_valid:
+        return True
+    else:
+        return False
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
---
+++ 7_regex/numb3rs/test_numb3rs.py
@@ -0,0 +1,27 @@
+from numb3rs import validate
+
+
+def test_valid_IPv4():
+    assert validate("192.168.0.1") is True
+    assert validate("10.0.0.1") is True
+    assert validate("172.16.0.1") is True
+    assert validate("255.255.255.255") is True
+    assert validate("0.0.0.0") is True
+    assert validate("127.0.0.1") is True
+    assert validate("203.0.113.0") is True
+    assert validate("198.51.100.0") is True
+    assert validate("100.64.0.1") is True
+    assert validate("169.254.0.1") is True
+
+
+def test_invalid_IPv4():
+    assert validate("256.0.0.1") is False      # (Octet value exceeds 255)
+    assert validate("300.200.100.0") is False  # (Octet value exceeds 255)
+    assert validate("192.168.0.0.1") is False  # (Too many octets)
+    assert validate("1.2.3.4.5") is False      # (Too many octets)
+    assert validate("300.168.1") is False      # (Too few octets)
+    assert validate("200.168.1.") is False     # (Too few octets)
+    assert validate("192.168.256.1") is False  # (Octet value exceeds 255)
+    assert validate("192.168.-1.1") is False   # (Negative value not allowed)
+    assert validate("192.168.1.256") is False  # (Octet value exceeds 255)
+    assert validate("192.168.01.1") is False   # (Leading zeros not allowed)
