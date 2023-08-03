7e73c9ad4b97867a6c228f183675e0fda6256c62
alex-anast <anastasioyaa@gmail.com>
2023-08-03 11:45:41 +0200

    reformat numb3rs with black

--- 7_regex/numb3rs/test_numb3rs.py
+++ 7_regex/numb3rs/test_numb3rs.py
@@ -15,13 +15,13 @@ def test_valid_IPv4():
 
 
 def test_invalid_IPv4():
-    assert validate("256.0.0.1") is False      # (Octet value exceeds 255)
+    assert validate("256.0.0.1") is False  # (Octet value exceeds 255)
     assert validate("300.200.100.0") is False  # (Octet value exceeds 255)
     assert validate("192.168.0.0.1") is False  # (Too many octets)
-    assert validate("1.2.3.4.5") is False      # (Too many octets)
-    assert validate("300.168.1") is False      # (Too few octets)
-    assert validate("200.168.1.") is False     # (Too few octets)
+    assert validate("1.2.3.4.5") is False  # (Too many octets)
+    assert validate("300.168.1") is False  # (Too few octets)
+    assert validate("200.168.1.") is False  # (Too few octets)
     assert validate("192.168.256.1") is False  # (Octet value exceeds 255)
-    assert validate("192.168.-1.1") is False   # (Negative value not allowed)
+    assert validate("192.168.-1.1") is False  # (Negative value not allowed)
     assert validate("192.168.1.256") is False  # (Octet value exceeds 255)
-    assert validate("192.168.01.1") is False   # (Leading zeros not allowed)
+    assert validate("192.168.01.1") is False  # (Leading zeros not allowed)
